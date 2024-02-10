f67527c456fa2a2115a1b21e11423f2e637b2341
wali131997 <70524307+wali131997@users.noreply.github.com>
2024-02-10 14:09:53 +0500

    fixed blog issue

--- resources/js/components/Frontend/Pages/Homepage/components/Blog/index.jsx
+++ resources/js/components/Frontend/Pages/Homepage/components/Blog/index.jsx
@@ -2,29 +2,29 @@ import React, { Fragment } from "react";
 function Blog() {
     return (
         <Fragment>
-            <section classname="news-style-two sec-pad">
-                <div classname="auto-container">
-                    <div classname="sec-title">
-                        <span classname="sub-title">News &amp; Updates</span>
+            <section className="news-style-two sec-pad">
+                <div className="auto-container">
+                    <div className="sec-title">
+                        <span className="sub-title">News &amp; Updates</span>
                         <h2>Recent Post From Our <span>Blog</span></h2>
-                        <div classname="link-box">
+                        <div className="link-box">
                             <a href="blog.html"><span>More News</span></a>
                         </div>
                     </div>
-                    <div classname="row clearfix">
-                        <div classname="col-lg-3 col-md-6 col-sm-12 news-block">
-                            <div classname="news-block-one wow fadeInUp animated" data-wow-delay="00ms" data-wow-duration="1500ms">
-                                <div classname="inner-box">
-                                    <div classname="image-box">
-                                        <figure classname="image"><a href="blog-details.html"><img src="assets/images/news/news-4.jpg" alt /></a></figure>
-                                        <span classname="post-date">24 Dec</span>
+                    <div className="row clearfix">
+                        <div className="col-lg-3 col-md-6 col-sm-12 news-block">
+                            <div className="news-block-one wow fadeInUp animated" data-wow-delay="00ms" data-wow-duration="1500ms">
+                                <div className="inner-box">
+                                    <div className="image-box">
+                                        <figure className="image"><a href="blog-details.html"><img src="assets/images/news/news-4.jpg" alt /></a></figure>
+                                        <span className="post-date">24 Dec</span>
                                     </div>
-                                    <div classname="lower-content">
+                                    <div className="lower-content">
                                         <h4><a href="blog-details.html">Best FTSE 250 shares to buy in Feb 2023</a></h4>
                                         <p>Error sit voluptatem accusantium doloremque...</p>
-                                        <div classname="author-box">
+                                        <div className="author-box">
                                             <h6>Trade Ideas</h6>
-                                            <ul classname="post-info clearfix">
+                                            <ul className="post-info clearfix">
                                                 <li><a href="blog-details.html">Justin Langer</a></li>
                                                 <li><span>..</span></li>
                                                 <li>2 Mins Read</li>
@@ -34,19 +34,19 @@ function Blog() {
                                 </div>
                             </div>
                         </div>
-                        <div classname="col-lg-3 col-md-6 col-sm-12 news-block">
-                            <div classname="news-block-one wow fadeInUp animated" data-wow-delay="200ms" data-wow-duration="1500ms">
-                                <div classname="inner-box">
-                                    <div classname="image-box">
-                                        <figure classname="image"><a href="blog-details.html"><img src="assets/images/news/news-5.jpg" alt /></a></figure>
-                                        <span classname="post-date">16 Dec</span>
+                        <div className="col-lg-3 col-md-6 col-sm-12 news-block">
+                            <div className="news-block-one wow fadeInUp animated" data-wow-delay="200ms" data-wow-duration="1500ms">
+                                <div className="inner-box">
+                                    <div className="image-box">
+                                        <figure className="image"><a href="blog-details.html"><img src="assets/images/news/news-5.jpg" alt /></a></figure>
+                                        <span className="post-date">16 Dec</span>
                                     </div>
-                                    <div classname="lower-content">
+                                    <div className="lower-content">
                                         <h4><a href="blog-details.html">Fixed vs floatin exchange rates main differences</a></h4>
                                         <p>Rerum facilis est et expedita distin ctio libero  Itaque earum...</p>
-                                        <div classname="author-box">
+                                        <div className="author-box">
                                             <h6>Economic</h6>
-                                            <ul classname="post-info clearfix">
+                                            <ul className="post-info clearfix">
                                                 <li><a href="blog-details.html">Mylah Sophia</a></li>
                                                 <li><span>..</span></li>
                                                 <li>3 Mins Read</li>
@@ -56,19 +56,19 @@ function Blog() {
                                 </div>
                             </div>
                         </div>
-                        <div classname="col-lg-3 col-md-6 col-sm-12 news-block">
-                            <div classname="news-block-one wow fadeInUp animated" data-wow-delay="400ms" data-wow-duration="1500ms">
-                                <div classname="inner-box">
-                                    <div classname="image-box">
-                                        <figure classname="image"><a href="blog-details.html"><img src="assets/images/news/news-6.jpg" alt /></a></figure>
-                                        <span classname="post-date">30 Nov</span>
+                        <div className="col-lg-3 col-md-6 col-sm-12 news-block">
+                            <div className="news-block-one wow fadeInUp animated" data-wow-delay="400ms" data-wow-duration="1500ms">
+                                <div className="inner-box">
+                                    <div className="image-box">
+                                        <figure className="image"><a href="blog-details.html"><img src="assets/images/news/news-6.jpg" alt /></a></figure>
+                                        <span className="post-date">30 Nov</span>
                                     </div>
-                                    <div classname="lower-content">
+                                    <div className="lower-content">
                                         <h4><a href="blog-details.html">Surprise move with widening of yield curve...</a></h4>
                                         <p>Temporibus autem quibusdam et aut debitis tenetur...</p>
-                                        <div classname="author-box">
+                                        <div className="author-box">
                                             <h6>Updates</h6>
-                                            <ul classname="post-info clearfix">
+                                            <ul className="post-info clearfix">
                                                 <li><a href="blog-details.html">Michael Rhys</a></li>
                                                 <li><span>..</span></li>
                                                 <li>2 Mins Read</li>
@@ -78,19 +78,19 @@ function Blog() {
                                 </div>
                             </div>
                         </div>
-                        <div classname="col-lg-3 col-md-6 col-sm-12 news-block">
-                            <div classname="news-block-one wow fadeInUp animated" data-wow-delay="600ms" data-wow-duration="1500ms">
-                                <div classname="inner-box">
-                                    <div classname="image-box">
-                                        <figure classname="image"><a href="blog-details.html"><img src="assets/images/news/news-7.jpg" alt /></a></figure>
-                                        <span classname="post-date">09 Nov</span>
+                        <div className="col-lg-3 col-md-6 col-sm-12 news-block">
+                            <div className="news-block-one wow fadeInUp animated" data-wow-delay="600ms" data-wow-duration="1500ms">
+                                <div className="inner-box">
+                                    <div className="image-box">
+                                        <figure className="image"><a href="blog-details.html"><img src="assets/images/news/news-7.jpg" alt /></a></figure>
+                                        <span className="post-date">09 Nov</span>
                                     </div>
-                                    <div classname="lower-content">
+                                    <div className="lower-content">
                                         <h4><a href="blog-details.html">World Finance and Bullion Trader on Market</a></h4>
                                         <p>Recusandae itaque earum rerum hic tenetur a sapiente...</p>
-                                        <div classname="author-box">
+                                        <div className="author-box">
                                             <h6>Trade Ideas</h6>
-                                            <ul classname="post-info clearfix">
+                                            <ul className="post-info clearfix">
                                                 <li><a href="blog-details.html">Haris Gulati</a></li>
                                                 <li><span>..</span></li>
                                                 <li>5 Mins Read</li>
