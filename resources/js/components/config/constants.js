e88d2ec150d3e53adc6eb40e32522d89095b5d8a
waleedse <70524307+waleedse@users.noreply.github.com>
2024-02-28 01:30:34 +0500

    w

--- resources/js/components/config/constants.js
+++ resources/js/components/config/constants.js
@@ -1,4 +1,4 @@
 
-export const  API_BASE_URL = 'http://localhost:8000/api/'
-export const  IMAGE_BASE_URL = 'http://localhost:8000/uploads/'
+export const  API_BASE_URL = 'https://verve.campusportal.pk/api/'
+export const  IMAGE_BASE_URL = 'https://verve.campusportal.pk/uploads/'
 
