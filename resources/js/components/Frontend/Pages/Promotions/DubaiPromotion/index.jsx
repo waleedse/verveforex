1c05423d927fc18db07ab94986efcf21d30b90bf
waleedse <70524307+waleedse@users.noreply.github.com>
2024-03-19 01:05:21 +0500

    w

--- resources/js/components/Frontend/Pages/Promotions/DubaiPromotion/index.jsx
+++ resources/js/components/Frontend/Pages/Promotions/DubaiPromotion/index.jsx
@@ -4,141 +4,141 @@ import PromotionRules from './components/PromotionRules'
 
 
 function DubaiPromotion() {
-  return (
-    <section className='promotion-wrapper'>
-      <div className='top-banner'>
-        <figure>
-          <img src="/assets/images/banner/TopBanner.jpg" />
-        </figure>
-      </div>
-      <div className="container">
-        <div className="promotion-heading">
-          <h3>Dubai Trip Offer</h3>
-          <p>Embark on an extraordinary journey to Dubai</p>
-        </div>
-        <div className="promotion-banner">
-          <figure>
-            <img src="/assets/images/banner/TripDetails.png" />
-          </figure>
-          <figure>
-            <img src="/assets/images/banner/HowtoParticipate.png" />
-          </figure>
-          <figure>
-            <img src="/assets/images/banner/OfferDetails.png" />
-          </figure>
-        </div>
-        <p className='promotion-details'>
-          Embark on an extraordinary journey to Dubai, where opulence meets opportunity with our exclusive trip as part of your forex trading. Prepare to immerse yourself in the lavish lifestyle of the city while exploring lucrative trading possibilities in one of the world's most dynamic financial hubs.
-        </p>
-        <p className='promotion-details'>
-          Your adventure begins with a warm welcome to Dubai, where you'll be accommodated in luxurious surroundings, providing the perfect blend of comfort and sophistication. Whether it's a sleek high-rise hotel offering stunning views of the city skyline or a lavish resort nestled along pristine beaches, your stay promises to be nothing short of extraordinary.
-        </p>
-        <p className='promotion-details'>
-          With the perfect blend of luxury and opportunity, our trip to Dubai offer promises an unforgettable experience that will leave you inspired and empowered to seize new trading opportunities in one of the world's most dynamic markets.
-        </p>
-      </div>
-      <div className='trip-details row'>
-          <div className='trip-description col-md-6'>
-            <h4>Trip Details</h4>
-            <p>
-              By taking part in this exclusive promotion, you have the opportunity to
-              win a truly luxurioustrip to Dubai, featuring:
-            </p>
-            <ul>
-              <li>
-                A Lavish 5 Days/4 Nights Luxury trip in Dubai for the lucky winner
-              </li>
-              <li>
-                Enjoy round-trip economy class airline tickets to the vibrant city of Dubai
-              </li>
-              <li>
-                Indulge in a lavish stay at a top-tier hotel for a specified number of nights
-              </li>
-              <li>
-                Visa processing fees for your visit to the United Arab Emirates are included
-              </li>
-              <li>
-                Explore Dubai's most sought-after attractions during your stay including a
-                visit to Burj Khalifa, Dessert Safari with Dinner and Entertainment, Dubai
-                City tour, Marina Dhow Cruise Dinner and more!
-              </li>
-              <li>
-                Daily Breakfast in the hotel
-              </li>
-              <li>
-                Airport pick up and drop off
-              </li>
-              <li>
-                Verve Forex will not cover additional expenses such as meals, personal
-                expenses, travel insurance, or any other cost not explicitly mentioned above
-              </li>
-            </ul>
-          </div>
-          <figure className='col-md-6'>
-            <img src="/assets/images/banner/Slide1.png" />
-          </figure>
-        </div>
-      <div className="promotion-heading">
-        <h3>How to participate in this offer?</h3>
-        <p>This is Exclusive offer for Verve Forex client’s and is valid for clients trading with the following Brokers:</p>
-      </div>
-      <div className='trip-details row'>
-        <figure className='col-md-6'>
-          <img src="/assets/images/banner/Slide2.png" />
-        </figure>
-        <div className='trip-description col-6'>
-          <h4>Offer Details</h4>
-          <p>
-            By taking part in this exclusive promotion, you have the opportunity to
-            win a truly luxurioustrip to Dubai, featuring:
-          </p>
-          <ol>
-            <li className='parent-list'>
-              If you are signing up with the above Brokers you must use one of the following
-              trading accounts:
-              <ul>
-                <li>If you are trading with Cabana Capitals, your account type must be either
-                  Standard or Ultra Account.</li>
-                <li>If you are trading with OctaFx your account type must be Octa Trader Account.</li>
-                <li>If you are trading with Exness your account type must be Standard Account.</li>
-                <li>If you are trading with Exclusive Markets your account type must be Standard
-                  or Standard Plus Account.</li>
-              </ul>
-            </li>
-            <li className='parent-list'>
-            A minimum trading volume of standard lots during the Promotion Period
-              <ul>
-                <li>
-                  600 Qualified (See Point X below for Qualified Lots) Standard lots to win a trip
-                  to Dubai for Single Person.
-                </li>
-                <li>
-                  1,000 qualified (See Point X below for Qualified Lots) Standard lots to win a trip
-                  to Dubai for a Couple.
-                </li>
-              </ul>
-            </li>
-            <li className='parent-list'>
-              Qualified lot means that the trade must be open for at least 3 minutes and the
-              difference between the opening and closing price must be at least 3 pips.
-            </li>
-            <li className='parent-list'>
-              As part of this great offer, Verve Forex also gives you an opportunity to combine
-              the above number of lots over multiple brokers e.g.
-              <ul>
-                <li>
-                  If you trade 400 lots with Cabana Capitals and 200 lots with OctaFx, you can still
-                  avail this offer. Please note that the accounts you are using for this offer are as
-                  per the account types listed above and also the Introducing Broker on your profile
-                  must be Verve Forex under all the Broker’s you will be using.
-                </li>
-              </ul>
-            </li>
-          </ol>
-        </div>
-      </div>
+    return (
+        <section className='promotion-wrapper'>
+            <div className='top-banner'>
+                <figure>
+                    <img src="/assets/images/banner/TopBanner.jpg" />
+                </figure>
+            </div>
+            <div className="container">
+                <div className="promotion-heading">
+                    <h3>Dubai Trip Offer</h3>
+                    <p className='text-center'> Embark on an extraordinary journey to Dubai</p>
+                </div>
+                <div className="promotion-banner">
+                    <figure>
+                        <img src="/assets/images/banner/TripDetails.png" />
+                    </figure>
+                    <figure>
+                        <img src="/assets/images/banner/HowtoParticipate.png" />
+                    </figure>
+                    <figure>
+                        <img src="/assets/images/banner/OfferDetails.png" />
+                    </figure>
+                </div>
+                <p className='promotion-details'>
+                    Embark on an extraordinary journey to Dubai, where opulence meets opportunity with our exclusive trip as part of your forex trading. Prepare to immerse yourself in the lavish lifestyle of the city while exploring lucrative trading possibilities in one of the world's most dynamic financial hubs.
+                </p>
+                <p className='promotion-details'>
+                    Your adventure begins with a warm welcome to Dubai, where you'll be accommodated in luxurious surroundings, providing the perfect blend of comfort and sophistication. Whether it's a sleek high-rise hotel offering stunning views of the city skyline or a lavish resort nestled along pristine beaches, your stay promises to be nothing short of extraordinary.
+                </p>
+                <p className='promotion-details'>
+                    With the perfect blend of luxury and opportunity, our trip to Dubai offer promises an unforgettable experience that will leave you inspired and empowered to seize new trading opportunities in one of the world's most dynamic markets.
+                </p>
+            </div>
+            <div className='trip-details row'>
+                <div className='trip-description col-md-6'>
+                    <h4>Trip Details</h4>
+                    <p>
+                        By taking part in this exclusive promotion, you have the opportunity to
+                        win a truly luxurioustrip to Dubai, featuring:
+                    </p>
+                    <ul>
+                        <li>
+                            A Lavish 5 Days/4 Nights Luxury trip in Dubai for the lucky winner
+                        </li>
+                        <li>
+                            Enjoy round-trip economy class airline tickets to the vibrant city of Dubai
+                        </li>
+                        <li>
+                            Indulge in a lavish stay at a top-tier hotel for a specified number of nights
+                        </li>
+                        <li>
+                            Visa processing fees for your visit to the United Arab Emirates are included
+                        </li>
+                        <li>
+                            Explore Dubai's most sought-after attractions during your stay including a
+                            visit to Burj Khalifa, Dessert Safari with Dinner and Entertainment, Dubai
+                            City tour, Marina Dhow Cruise Dinner and more!
+                        </li>
+                        <li>
+                            Daily Breakfast in the hotel
+                        </li>
+                        <li>
+                            Airport pick up and drop off
+                        </li>
+                        <li>
+                            Verve Forex will not cover additional expenses such as meals, personal
+                            expenses, travel insurance, or any other cost not explicitly mentioned above
+                        </li>
+                    </ul>
+                </div>
+                <figure className='col-md-6'>
+                    <img src="/assets/images/banner/Slide1.png" />
+                </figure>
+            </div>
+            <div className="promotion-heading">
+                <h3>How to participate in this offer?</h3>
+                <p>This is Exclusive offer for Verve Forex client’s and is valid for clients trading with the following Brokers:</p>
+            </div>
+            <div className='trip-details row'>
+                <figure className='col-md-6'>
+                    <img src="/assets/images/banner/Slide2.png" />
+                </figure>
+                <div className='trip-description col-6'>
+                    <h4>Offer Details</h4>
+                    <p>
+                        By taking part in this exclusive promotion, you have the opportunity to
+                        win a truly luxurioustrip to Dubai, featuring:
+                    </p>
+                    <ol>
+                        <li className='parent-list'>
+                            If you are signing up with the above Brokers you must use one of the following
+                            trading accounts:
+                            <ul>
+                                <li>If you are trading with Cabana Capitals, your account type must be either
+                                    Standard or Ultra Account.</li>
+                                <li>If you are trading with OctaFx your account type must be Octa Trader Account.</li>
+                                <li>If you are trading with Exness your account type must be Standard Account.</li>
+                                <li>If you are trading with Exclusive Markets your account type must be Standard
+                                    or Standard Plus Account.</li>
+                            </ul>
+                        </li>
+                        <li className='parent-list'>
+                            A minimum trading volume of standard lots during the Promotion Period
+                            <ul>
+                                <li>
+                                    600 Qualified (See Point X below for Qualified Lots) Standard lots to win a trip
+                                    to Dubai for Single Person.
+                                </li>
+                                <li>
+                                    1,000 qualified (See Point X below for Qualified Lots) Standard lots to win a trip
+                                    to Dubai for a Couple.
+                                </li>
+                            </ul>
+                        </li>
+                        <li className='parent-list'>
+                            Qualified lot means that the trade must be open for at least 3 minutes and the
+                            difference between the opening and closing price must be at least 3 pips.
+                        </li>
+                        <li className='parent-list'>
+                            As part of this great offer, Verve Forex also gives you an opportunity to combine
+                            the above number of lots over multiple brokers e.g.
+                            <ul>
+                                <li>
+                                    If you trade 400 lots with Cabana Capitals and 200 lots with OctaFx, you can still
+                                    avail this offer. Please note that the accounts you are using for this offer are as
+                                    per the account types listed above and also the Introducing Broker on your profile
+                                    must be Verve Forex under all the Broker’s you will be using.
+                                </li>
+                            </ul>
+                        </li>
+                    </ol>
+                </div>
+            </div>
 
-    
+
             <PromotionRules />
         </section>
     )
