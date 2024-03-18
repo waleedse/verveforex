e2fe5aa2fc740a4269ba5349bfbcab617e38cf1a
TalhaNazim <talha.hasnain.nazim@gmail.com>
2024-03-18 23:06:49 +0500

    Wroking on dubai promotion

--- resources/js/components/Frontend/Pages/Promotions/DubaiPromotion/index.jsx
+++ resources/js/components/Frontend/Pages/Promotions/DubaiPromotion/index.jsx
@@ -3,9 +3,29 @@ import '../promotions.css'
 
 function DubaiPromotion() {
   return (
-    <div style={{marginTop:"140px"}}>
-      helo
-    </div>
+    <section className='promotion-wrapper'>
+      <div className='top-banner'>
+        <figure>
+          <img src="/assets/images/banner/TopBanner.jpg" />
+        </figure>
+      </div>
+      <div className="promotion-heading">
+        <h3>Dubai Trip Offer</h3>
+        <p>Embark on an extraordinary journey to Dubai</p>
+      </div>
+      <div className="promotion-banner">
+        <figure>
+          <img src="/assets/images/banner/TripDetails.png"/>
+        </figure>
+        <figure>
+          <img src="/assets/images/banner/HowtoParticipate.png"/>
+        </figure>
+        <figure>
+          <img src="/assets/images/banner/OfferDetails.png"/>
+        </figure>
+      </div>
+
+    </section>
   )
 }
 
--- resources/js/components/Frontend/Pages/static/PrivacyPolicy/index.jsx
+++ resources/js/components/Frontend/Pages/static/PrivacyPolicy/index.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+
 function PrivacyPolicy() {
     return (
         <Fragment>
@@ -15,6 +16,20 @@ function PrivacyPolicy() {
                     </div>
                 </div>
             </section>
+            <ul style={{padding:"30px 150px", color:"Black"}}>
+                <li style={{listStyle:"romen"}}>
+                    Introduction:
+                    <ul>
+                        <li style={{listStyle:"disc"}}> This Legal Notice Policy governs the use of the Verve Forex website (the "Website"). By accessing or using the Website, you agree to comply with this Legal Notice Policy and all applicable laws and regulations.</li>
+                    </ul>
+                </li>
+                <li>
+                    Copyright Notice:
+                    <ul>
+                        <li>. Unless otherwise indicated, all content on the Website, including text, graphics, logos, images, and software, is the property of Verve Forex or its licensors and is protected by copyright laws.</li>
+                    </ul>
+                </li>
+            </ul>
 
         </Fragment>
     );
