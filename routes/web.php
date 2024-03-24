bbf397c68c645776b9ff3e5756f031756e4a222c
waleedse <70524307+waleedse@users.noreply.github.com>
2024-03-25 01:28:30 +0500

    w

--- routes/web.php
+++ routes/web.php
@@ -19,14 +19,14 @@ Route::get('/email/verify', function () {
     return view('auth.verify-email');
 })->middleware('auth')->name('verification.notice');
 
-Route::get('/emails/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
 
     return redirect('/home');
 })->middleware(['auth', 'signed'])->name('verification.verify');
 
 Route::post('/email/verification-notification', function (Request $request) {
-    // $request->user()->sendEmailVerificationNotification();
+    $request->user()->sendEmailVerificationNotification();
 
     return back()->with('message', 'Verification link sent!');
 })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
@@ -36,7 +36,7 @@ Route::get('/admin-login', function () {return view('admin');});
 Route::get('/login', function () {return view('client');});
 Route::get('/signup', function () {return view('client');});
 Route::get('/client/{path?}', function () {return view('admin');})->where('path' , '.*');
-Route::get('/email/verify/{id}/{hash}', function () {return view('client');});
+// Route::get('/email/verify/{id}/{hash}', function () {return view('client');});
 
 Route::view('/{path?}', 'welcome');
 Route::get('/{path?}', function () {return view('welcome');})->where('path' , '.*');
