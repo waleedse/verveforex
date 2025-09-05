f7c2b13286d0b445e65d8fb36a618bd2550034a0
Rushda-Rasheede <rushirasheed42@gmail.com>
2025-09-06 03:38:49 +0500

    w

--- resources/js/components/config/constants.js
+++ resources/js/components/config/constants.js
@@ -3,5 +3,5 @@
 // export const IMAGE_BASE_URL = 'http://localhost:8000/uploads/'
 // export const API_BASE_URL = 'https://verve.campusportal.com/api/'
 // export const IMAGE_BASE_URL = 'https://verve.campusportal.com/uploads/'
-export const API_BASE_URL = 'https://verveforex.com/api/'
-export const IMAGE_BASE_URL = 'https://verveforex.com/uploads/'
+export const API_BASE_URL = 'https://qondzo.com/api/'
+export const IMAGE_BASE_URL = 'https://qondzo.com/uploads/'
